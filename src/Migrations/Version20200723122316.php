cde0402283fdf7d1acdd4526067e1483a14aae7f
cedricrouxcedric <cedricroux@outlook.fr>
2020-07-23 17:18:03 +0200

    creation de l'entité User

--- src/Migrations/Version20200723122316.php
+++ src/Migrations/Version20200723122316.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200713132814 extends AbstractMigration
+final class Version20200723122316 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,8 +22,7 @@ final class Version20200713132814 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-//        $this->addSql('CREATE TABLE images (id INT AUTO_INCREMENT NOT NULL, moto_id INT NOT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_E01FBE6A78B8F2AC (moto_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-//        $this->addSql('ALTER TABLE images ADD CONSTRAINT FK_E01FBE6A78B8F2AC FOREIGN KEY (moto_id) REFERENCES moto (id)');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
@@ -31,6 +30,6 @@ final class Version20200713132814 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE images');
+        $this->addSql('DROP TABLE user');
     }
 }
